8e957e07ef9f57648c63f2a3850b0391580aebbd
koonwen <koonwen@gmail.com>
2021-06-15 22:59:55 +0800

    Add in secret key

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # Create and configure app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask('flaskr', instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
