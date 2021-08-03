1956e579fed30061650b632d442150f27c1876de
koonwen <koonwen@gmail.com>
2021-08-03 18:09:49 +0800

    Add configs and packaging for travis ci.

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(name="app", packages=find_packages())
-
+setup(
+    name='app',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
 # TODO Package Properly
 # TODO ADD CI/CD
 # TODO Reivew pushing rights
