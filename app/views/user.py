7bb5f5d94da33e2d314adb31cfad8e442ae7dd9a
koonwen <koonwen@gmail.com>
2021-10-20 21:07:41 +0800

    Change number of people recorded to draw from the last 5 minutes and return the max * 2

--- app/views/user.py
+++ app/views/user.py
@@ -2,13 +2,17 @@ from flask import Blueprint, render_template
 from app.db.models import Data
 bp = Blueprint('user', __name__)
 
+# This view returns the maximum number of people recorded in the gym over a 5 minute interval
+# The number is multiplied by 2 to better estimate the actual number of people given the constraints of the
+# camera set up
 @bp.route('/')
 def index():
-    latest = Data.get_latest_entry()
-    if not latest:
-        average="No data yet"
+    latest5 = Data.get_latest_n_entries(5)
+    if not latest5:
+        highest="No data yet"
         timestamp="Nil"
     else:
-        average = latest.mode
-        timestamp = latest.timestamp.isoformat(sep=' ', timespec='seconds')
-    return render_template('index.html', number=average, timestamp=timestamp)
+        hi_list = map(lambda entry: entry.highest, latest5)
+        highest = max(hi_list) * 2
+        timestamp = latest5[4].timestamp.isoformat(sep=' ', timespec='seconds')
+    return render_template('index.html', number=highest, timestamp=timestamp)
