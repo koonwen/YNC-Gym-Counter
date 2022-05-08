104e15259a8a138ceb111f9d7d895d66e180cb3a
koonwen <koonwen@gmail.com>
2022-05-08 12:29:13 +0800

    update people count heuristic to be more conservative

--- app/views/user.py
+++ app/views/user.py
@@ -3,8 +3,6 @@ from app.db.models import Data
 bp = Blueprint('user', __name__)
 
 # This view returns the maximum number of people recorded in the gym over a 5 minute interval
-# The number is multiplied by 2 to better estimate the actual number of people given the constraints of the
-# camera set up
 @bp.route('/')
 def index():
     latest5 = Data.get_latest_n_entries(5)
@@ -13,6 +11,6 @@ def index():
         timestamp="Nil"
     else:
         hi_list = map(lambda entry: entry.highest, latest5)
-        highest = max(hi_list) * 2
+        highest = max(hi_list)
         timestamp = latest5[4].timestamp.isoformat(sep=' ', timespec='seconds')
     return render_template('index.html', number=highest, timestamp=timestamp)
